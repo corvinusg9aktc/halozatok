b317fc3e353ceabbfa61674039d18b3383d2ce44
Felhasználó <Felhasznl@DESKTOP-LHR835G>
2021-04-15 12:03:00 +0200

    nyolcadik gyakorlat

---
+++ 5/otodikora/otodikora/Controllers/BoatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using otodikora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace otodikora.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdések = from x in context.Questions select x.QuestionText;
+
+            return new JsonResult(kérdések);
+        }
+    }
+}
--- 5/otodikora/otodikora/Startup.cs
+++ 5/otodikora/otodikora/Startup.cs
@@ -16,6 +16,7 @@ namespace otodikora
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,11 @@ namespace otodikora
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
