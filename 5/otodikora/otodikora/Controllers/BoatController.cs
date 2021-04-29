f936fc5ff6329f103db836b2a65b4d1ab6ce43d6
Felhasználó <Felhasznl@DESKTOP-LHR835G>
2021-04-29 12:49:16 +0200

    kilencedikgyak

--- 5/otodikora/otodikora/Controllers/BoatController.cs
+++ 5/otodikora/otodikora/Controllers/BoatController.cs
@@ -21,5 +21,20 @@ namespace otodikora.Controllers
 
             return new JsonResult(kérdések);
         }
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
     }
+
+
 }
--- 5/otodikora/otodikora/wwwroot/hetedik.js
+++ 5/otodikora/otodikora/wwwroot/hetedik.js
@@ -1,71 +1,100 @@
-﻿var kerdesek;
-var haladas = 0;
+﻿//var kerdesek;
+var haladas = 1;
+var ajdi = 0;
+var szin;
 
 window.onload = () => {
     letoltes()
 }
 
 function letoltes() {
-    fetch('questions.json').then(r => r.json()).then(d => letöltésBefejeződött(d));
+    //fetch('/questions/all').then(r => r.json()).then(d => letöltésBefejeződött(d));
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kerdesMegjelenites(data)
+        );
 }
 
-function letöltésBefejeződött(d) {
-    console.log("Sikeres letöltés")
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
     console.log(d)
     kerdesek = d;
-    kerdesMegjelenites(0);
+    //kerdesMegjelenites(0);
 }
 
 function kerdesMegjelenites(k) {
-    document.getElementById("kérdés_szöveg").innerHTML = kerdesek[k].questionText;
-    document.getElementById("válasz1").innerHTML = kerdesek[k].answer1;
-    document.getElementById("válasz2").innerHTML = kerdesek[k].answer2;
-    document.getElementById("válasz3").innerHTML = kerdesek[k].answer3;
-    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kerdesek[k].image;
+    console.log(k);
+    szin = k;
+    /*document.getElementById("kérdés_szöveg").innerHTML = kerdesek[k].questionText;
+    document.getElementById("válasz1").innerHTML = kerdesek[k].answer1;
+    document.getElementById("válasz2").innerHTML = kerdesek[k].answer2;
+    document.getElementById("válasz3").innerHTML = kerdesek[k].answer3;
+    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kerdesek[k].image;*/
+    document.getElementById("kérdés_szöveg").innerHTML = k.questionText;
+    document.getElementById("válasz1").innerHTML = k.answer1;
+    document.getElementById("válasz2").innerHTML = k.answer2;
+    document.getElementById("válasz3").innerHTML = k.answer3;
+    if (k.image != "") {
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + k.image;
+    } else {
+        document.getElementById("kép1").src = "";
+    }
 }
 
 function elore() {
-    document.getElementById("válasz1").style.backgroundColor = "white"
-    document.getElementById("válasz2").style.backgroundColor = "white"
-    document.getElementById("válasz3").style.backgroundColor = "white"
+    document.getElementById("válasz1").style.backgroundColor = "white"
+    document.getElementById("válasz2").style.backgroundColor = "white"
+    document.getElementById("válasz3").style.backgroundColor = "white"
     haladas++
-    if (haladas == kerdesek.length) {
-        haladas = 0;
+    if (haladas == 860) {
+        haladas = 1;
     }
-    kerdesMegjelenites(haladas);
+    kérdésBetöltés(haladas);
 }
 
 function vissza() {
-    document.getElementById("válasz1").style.backgroundColor = "white"
-    document.getElementById("válasz2").style.backgroundColor = "white"
-    document.getElementById("válasz3").style.backgroundColor = "white"
+    document.getElementById("válasz1").style.backgroundColor = "white"
+    document.getElementById("válasz2").style.backgroundColor = "white"
+    document.getElementById("válasz3").style.backgroundColor = "white"
+    haladas--;
     if (haladas == 0) {
-        haladas = 2;
-        kerdesMegjelenites(haladas)
-    } else {
-        haladas--;
-        kerdesMegjelenites(haladas);
+        haladas = 859;
+        //kérdésBetöltés(haladas);
     }
-    kerdesMegjelenites(haladas);
+    kérdésBetöltés(haladas);
+
 }
 
-function valasz() {
-    var jovalasz = kérdések[haladás].correctAnswer
-    // console.log(jovalasz)
+function szinez() {
+    var jovalasz = szin.correctAnswer;
+    //console.log(jovalasz)
     if (jovalasz == 1) {
-        document.getElementById("válasz1").style.backgroundColor = "green"
-        document.getElementById("válasz2").style.backgroundColor = "red"
-        document.getElementById("válasz3").style.backgroundColor = "red"
+        document.getElementById("válasz1").style.backgroundColor = "green"
+        document.getElementById("válasz2").style.backgroundColor = "red"
+        document.getElementById("válasz3").style.backgroundColor = "red"
     }
     else if (jovalasz == 2) {
-        document.getElementById("válasz1").style.backgroundColor = "red"
-        document.getElementById("válasz2").style.backgroundColor = "green"
-        document.getElementById("válasz3").style.backgroundColor = "red"
+        document.getElementById("válasz1").style.backgroundColor = "red"
+        document.getElementById("válasz2").style.backgroundColor = "green"
+        document.getElementById("válasz3").style.backgroundColor = "red"
     }
     else {
-        document.getElementById("válasz1").style.backgroundColor = "red"
-        document.getElementById("válasz2").style.backgroundColor = "red"
-        document.getElementById("válasz3").style.backgroundColor = "green"
+        document.getElementById("válasz1").style.backgroundColor = "red"
+        document.getElementById("válasz2").style.backgroundColor = "red"
+        document.getElementById("válasz3").style.backgroundColor = "green"
     }
+}
 
+function kérdésBetöltés(id) {
+    ajdi = id;
+    fetch(/questions/${ ajdi })
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${ response.status }`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kerdesMegjelenites(data));
 }
