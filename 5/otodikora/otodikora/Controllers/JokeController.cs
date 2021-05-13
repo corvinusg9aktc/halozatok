b26684cab32a25788af95f91d7bc6aab7adbaa64
Felhasználó <Felhasznl@DESKTOP-LHR835G>
2021-05-13 12:36:51 +0200

    tizenkettedikgyakorlat

---
+++ 5/otodikora/otodikora/Controllers/JokeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using otodikora.JokeModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace otodikora.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            return keresettVicc;
+        }
+
+        // POST api/<JokeController>
+        [HttpPost]
+        public void Post([FromBody] Joke újVicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JokeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+        }
+    }
+}
